755f1c6b9ead3963190ab4f6e4a4a6a7eed92b62
Roman Baukin <rbaukin1984@gmail.com>
2020-12-21 15:26:28 +0400

    Добавил замену фото и запрет ввода букв

--- script/script.js
+++ script/script.js
@@ -256,4 +256,38 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   slider();
+
+  //Меняем фото при наведении
+
+  const changeFoto = () => {
+    const img = document.querySelectorAll('.command__photo');
+    let src;
+
+    img.forEach((item) => {
+      item.addEventListener('mouseenter', (event) => {
+        src = event.target.src;
+        event.target.src = event.target.dataset.img;
+      });
+
+      item.addEventListener('mouseleave', (event) => {
+        event.target.src = src;
+      });
+    });
+  };
+
+  changeFoto();
+
+  // Калькулятор
+
+  const calculator = () => {
+    const input = document.querySelectorAll('input.calc-item');
+
+    input.forEach((item) => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/\D/g, '');
+      });
+    });
+  };
+
+  calculator();
 });
