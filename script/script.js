0670f43eed02ec332081d270876ed13ee1c9d5e2
Roman Baukin <rbaukin1984@gmail.com>
2021-01-05 16:19:27 +0400

    Урок №19

--- script/script.js
+++ script/script.js
@@ -100,4 +100,29 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   togglePopUp();
+
+  // Плавный скролл
+
+  const smoothScrolling = () => {
+    const links = document.querySelectorAll('.smooth-scroll');
+
+    links.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let target = event.target;
+        if (!target.getAttribute('href')) {
+          target = target.closest('.smooth-scroll');
+        }
+        const blockID = target.getAttribute('href').substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      });
+    });
+  };
+
+  smoothScrolling();
 });
